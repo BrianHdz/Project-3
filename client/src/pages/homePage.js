7d0a8a02c7274e5d85cb0dc10bcae01b684490fe
Mafry Rivera <mafryfernanda@gmail.com>
2020-04-20 16:05:39 -0700

    new

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import StackGrid from "react-stack-grid";
-import ylogo from "../images/youtube .png";
-import slogo from "../images/spotify.png";
+import ylogo from "../images/youtubes.png";
+import slogo from "../images/spotifys.png";
+import tlogo from "../images/twitters.png";
 import Nav from "../components/Nav/index";
 import { render } from "react-dom";
 
@@ -9,26 +10,34 @@ import { render } from "react-dom";
 function HomePage() {
 
       return (
+        
         <div>
           <Nav/>
-        <StackGrid columnWidth={150}>
+          <div class="a"></div>
+          <div>
+            <StackGrid columnWidth={100} >
+                  <div></div>
+                  <div></div>
+              <a href="http://localhost:3000/youtubePage"><button type="button">YouTube <img src={ylogo} alt="Logo"/></button></a>
+                  <div></div>
+                  <div></div>
+                  <div></div>
 
-          <div key="key1">Youtube</div>
-            <img src={ylogo} alt="Logo"/>
-                <div onMouseDown={ e => e.stopPropagation() }>
-         </div>
+              <a href="http://localhost:3000/spotifyPage"><button type="button">Spotify<img src={slogo} alt="Logo"/></button></a>
+                  <div></div>
+                  <div></div>
+                  <div></div>
 
-          <div key="key2">Spotify</div>
-            <img src={slogo} alt="Logo"/>  
-              <div onMouseDown={ e => e.stopPropagation() }>
+              <a href="http://localhost:3000/twitterPage"><button type="button">Twitter<img src={tlogo} alt="Logo"/></button></a>
+                  <div></div>
+            </StackGrid>
+          </div>
 
-                  </div>
-        </StackGrid>
-
-          <StackGrid columnWidth={50}>
-        <div key="key1">Item 1</div>
-        <div key="key3">Item 3</div>
-        </StackGrid>
+{/*         <StackGrid columnWidth={50}>
+          <div key="key1">Item 1</div>
+          <div key="key2">Item 2</div>
+          <div key="key3">Item 3</div>
+        </StackGrid> */}
               </div>
      )
 }
