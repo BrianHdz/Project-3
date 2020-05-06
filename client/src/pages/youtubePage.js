24f5703b66a940a600ae6c4e5ce0433f9f9fe209
toritheterrible <torimpham@gmail.com>
2020-05-05 21:44:03 -0700

    added new front end elements

--- client/src/pages/youtubePage.js
+++ client/src/pages/youtubePage.js
@@ -2,12 +2,14 @@ import React from "react";
 import Nav from "../components/Nav/index";
 import Index from "../components/YTlayout/Index";
 import { Provider } from "../context";
+import JumboNav from "../components/jumboNav"
 
 function YoutubePage() {
   return (
     <Provider>
       <React.Fragment>
-        <Nav />
+        {/* <Nav /> */}
+        <JumboNav/>
         <div className="container">
           <Index />
         </div>
